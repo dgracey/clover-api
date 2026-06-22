c7710b2dd2cf9e331945687d39d3d0c0c68c907c
dgracey <dgracey@nevergravity.com>
2026-06-22 12:17:08 -0500

    Add retry payment link (previously only on live site)

--- templates/payment-template.php
+++ templates/payment-template.php
@@ -73,7 +73,7 @@
         <p style="text-align:center" id="message"></p>
 
         <div id="footer">
-            <p>You have not been charged for this transaction.</p>
+            <p>You have not been charged for this transaction.</p><p><a href="https://midwesttile.com/card-payment/">Try your payment again</a></p>
         </div>
         </div>
     </div>
